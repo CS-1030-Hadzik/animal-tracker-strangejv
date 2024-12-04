75dc68eb61ad7dcb0dea6945603833020d3bae31
strangejv <7vjavster@gmail.com>
2024-12-03 18:32:09 -0700

    finished animal tab

--- animal.py
+++ animal.py
@@ -4,14 +4,25 @@ class Animal:
     """
     # Class-level attribute
     kingdom = "Animalia"
+
     # TODO create a class-level attribute that is a list of all the Animal objects
+    all_animals = []
 
     # TODO create the initializer for Animal with name and species as attributs
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+
+        Animal.all_animals.append(self)
 
     # TODO: Add a method to make a generic sound 
     # Call the method `speak` and make it output a specific message like 
     # "The animal makes a noise.""
+    def speak(self):
+        print("Then animal makes a noise")
 
     # TODO __str__ method for string representation
     # Example output
     # Kingdom: 'kingdom attribute', Name: 'name attribute' Species: 'species attribute' 
+    def __str__(self):
+        return f"Kingdom: '{self.kingdom}', Name: '{self.name}' Species: '{self.species}'"
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 
 if __name__ == "__main__":
     # TODO: Create an instance of the Animal class
+    class Animal:
+        def _init_(self,species)
     # TODO: Print the Animal instance
     # TODO: Call the method to make a generic animal sound
 
